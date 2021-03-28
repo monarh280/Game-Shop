fe608a7b0b25680cc9a74baa11464a9e899c6977
monarh280 <stipositi@gmail.com>
2021-03-28 14:25:55 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.h-slider').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000
+
+    });
+
+    $('.catalog-slider').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500
+
+    });
+  })
+
+
